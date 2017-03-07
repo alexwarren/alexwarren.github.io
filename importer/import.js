415bb3201ecc96fd3c7e7e38b878a8c2e47ca452
Alex Warren <alex@textadventures.co.uk>
2017-03-07 19:30:20 +0000

    Importer

--- importer/import.js
+++ importer/import.js
@@ -8,17 +8,42 @@ var fs = require("fs");
 var parseString = require("xml2js").parseString;
 var xml = fs.readFileSync(xmlFile);
 
+var pad = function (n) {
+    if (n > 9) return "" + n;
+    return "0" + n;
+};
+
 parseString(xml, function (err, result) {
     result.rss.channel[0].item.forEach(function(element) {
-        var postType = element["wp:post_type"][0];
+        var get = function (attr) {
+            return element[attr][0];
+        };
+
+        var postType = get("wp:post_type");
         if (postType !== "page" && postType !== "post") return;
+        if (get("wp:status") !== "publish") return;
         
-        console.log(element.title[0]);
-        console.log(element["wp:post_date"][0]);
-        console.log(element["wp:post_name"][0]);
-        console.log(element["wp:post_type"][0]);
+        console.log("\n\n");
+        console.log(get("title"));
+        console.log(get("wp:post_date"));
+        console.log(get("wp:post_name"));   // slug
+        //console.log(get("content:encoded"));
 
         //console.log(element);
-        console.log("\n\n");
+
+        var date = new Date(get("wp:post_date"));
+        var filename = `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}-${get("wp:post_name")}.md`;
+        var permalink = `/${date.getFullYear()}/${pad(date.getMonth()+1)}/${pad(date.getDate())}/${get("wp:post_name")}/`;
+        console.log(filename);
+        console.log(permalink);
+
+        var content = `---
+layout: post
+title: ${get("title")}
+permalink: ${permalink}
+---
+${get("content:encoded")}`;
+
+        fs.writeFileSync(`../_posts/${filename}`, content);
     }, this);
 });
