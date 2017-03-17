87915d77643c2b5840295a84a40d27472bdd7e3f
Alex Warren <alex@textadventures.co.uk>
2017-03-17 18:40:21 +0000

    Save downloaded files

--- importer/images.js
+++ importer/images.js
@@ -6,50 +6,54 @@ var re = /(https?:[^\s"\<]+)/g;
 var max = 500;
 var count = 0;
 
-var getUrl = function (c, url) {
+var getUrl = function (c, url, file) {
     try {
-        console.log(`${c} ${url}`);
+        //console.log(`${c} ${url}`);
         http.get(url, (res) => {
             const statusCode = res.statusCode;
             const contentType = res.headers['content-type'];
 
             if (statusCode !== 200) {
-                console.log(`${c} ${statusCode} ${url}`);
+                //console.log(`${c} ${statusCode} ${url}`);
                 // consume response data to free up memory
                 res.resume();
                 return;
             }
 
-            if (statusCode !== 200 || !/^image\//.test(contentType)) {
-                console.log(`${c} ${contentType} ${url}`);
+            if (!/^image\//.test(contentType)) {
+                //console.log(`${c} ${contentType} ${url}`);
                 // consume response data to free up memory
                 res.resume();
                 return;
             }
 
-            console.log(`${c} FETCHING ${url}`);
+            //console.log(`${c} FETCHING ${url}`);
 
             let rawData = [];
             res.on('data', (chunk) => {
-                console.log(`${c} got data`);
+                //console.log(`${c} got data`);
                 rawData.push(chunk)
             });
             res.on('end', () => {
                 var buffer = Buffer.concat(rawData);
-                console.log(buffer.toString('base64'));
+                //console.log(buffer.toString('base64'));
+                console.log(`${url} SUCCESS for ${file}`);
+
+                fs.writeFileSync("../images/" + url.replace(/https?:\/\//, "").replace(/\//g, "-"), buffer);
             });
         }).on('error', (e) => {
-            console.log(`${c} Got error: ${e.message}`);
+            //console.log(`${url} Got error: ${e.message}`);
         });
     }
     catch (e) {
-        console.log(`${c} ERROR ${e}`);
+        //console.log(`${c} ERROR ${e}`);
     }
 }
 
 fs.readdir("../_posts", (err, files) => {
-    files.forEach(file => {
-        fs.readFile("../_posts/" + file, (err, data) => {
+    files.forEach(filename => {
+        var file = "../_posts/" + filename;
+        fs.readFile(file, (err, data) => {
             if (err) throw err;
             var content = data.toString();
             
@@ -57,7 +61,7 @@ fs.readdir("../_posts", (err, files) => {
                 var m = re.exec(content);
                 if (m) {
                     if (count++ > max) break;
-                    getUrl(count, m[1]);
+                    getUrl(count, m[1], file);
                 }
             } while (m && count < max);
         });
